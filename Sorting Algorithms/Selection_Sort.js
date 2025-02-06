8c9a31c458efe6a7385bf9702e0df0e1ce4fac66
Rajat Sharan <errajatsharan@gmail.com>
2025-02-06 09:36:24 +0530

    Selection_Sort

--- Sorting Algorithms/Selection_Sort.js
+++ Sorting Algorithms/Selection_Sort.js
@@ -1,23 +1,19 @@
-function selectSort(arr){
-    const swap=(arr,idx,idx2)=>
-    {
-        ([arr[idx],arr[idx2]]=[arr[idx2],arr[idx]])
+function selectSort(arr) {
+    const swap = (arr, idx, idx2) => {
+        ([arr[idx], arr[idx2]] = [arr[idx2], arr[idx]])
     }
-    for(let i = 0 ; i < arr.length ; i++)
-    {
+    for (let i = 0; i < arr.length; i++) {
         let lowest = i
-        for(let j = i + 1 ; j < arr.length ; j++)
-        {
-        if(arr[j]<arr[lowest])
-            {
-            lowest=j
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[lowest]) {
+                lowest = j
             }
-            
+
         }
-        if(i!==lowest) swap(arr,i,lowest)
+        if (i !== lowest) swap(arr, i, lowest)
     }
-return arr
+    return arr
 
 }
 
-console.log(selectSort([34,22,10,19,17])) // [10, 17, 19, 22, 34]
+console.log(selectSort([34, 22, 10, 19, 17])) // [10, 17, 19, 22, 34]
